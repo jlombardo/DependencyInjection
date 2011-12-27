a36acf0a4ee09dc7d0e97547385d003bba7dad0c
Jim Lombardo <james.g.lombardo@gmail.com>
2011-12-27 08:07:31 -0600

    Fixed bug in properites file

--- src/dependencyinjection/example2/MessageInput.java
+++ src/dependencyinjection/example2/MessageInput.java
@@ -1,9 +1,5 @@
 package dependencyinjection.example2;
 
-/**
- *
- * @author jlombardo
- */
 public interface MessageInput {
 
     String getMessage();
--- src/dependencyinjection/example2/MessageOutput.java
+++ src/dependencyinjection/example2/MessageOutput.java
@@ -1,9 +1,5 @@
 package dependencyinjection.example2;
 
-/**
- *
- * @author jlombardo
- */
 public interface MessageOutput {
 
     void outputMessage(String message);
